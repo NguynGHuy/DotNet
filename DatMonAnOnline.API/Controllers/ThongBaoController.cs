e09eecc186234d74f26d012e784eed8037e505c1
NguynGHuy <nguyenhuy33644@gmail.com>
2026-09-21 15:13:38 +0700

    cmit

---
+++ DatMonAnOnline.API/Controllers/ThongBaoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatMonAnOnline.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ThongBaoController : ControllerBase
+    {
+    }
+}
