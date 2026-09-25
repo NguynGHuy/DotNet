c5c95e34aa43fa4b3c750839e5d29586e52caff4
NguynGHuy <nguyenhuy33644@gmail.com>
2026-09-25 12:57:03 +0700

    .

--- DatMonAnOnline.API/Program.cs
+++ DatMonAnOnline.API/Program.cs
@@ -6,6 +6,16 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 
 // =========================
 // DATABASE
@@ -114,6 +124,8 @@ if (app.Environment.IsDevelopment())
 // =========================
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowFrontend");
 
 app.UseAuthentication();
 
